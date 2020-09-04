618507f61eab26ab03db10e3881ec4831b0342fb
colinporth <colinporth@users.noreply.github.com>
2020-09-04 15:11:16 +0100

    Update lcdTest.cpp

--- lcdTest.cpp
+++ lcdTest.cpp
@@ -18,6 +18,7 @@ int main (int numArgs, char* args[]) {
   for (int i = 1; i < numArgs; i++)
     argStrings.push_back (args[i]);
 
+  bool fontTest = false;
   cLcd::eRotate rotate = cLcd::e0;
   cLcd::eInfo info = cLcd::eNone;
   cLcd::eMode mode = cLcd::eAll;
@@ -30,35 +31,38 @@ int main (int numArgs, char* args[]) {
     else if (argStrings[i] == "a") mode = cLcd::eAll;
     else if (argStrings[i] == "c") mode = cLcd::eCoarse;
     else if (argStrings[i] == "e") mode = cLcd::eExact;
+    else if (argStrings[i] == "f") fontTest = true;
 
   //cLcd* lcd = new cLcdIli9320 (argStrings.empty() ? 270 : stoi (argStrings[0]));
   cLcd* lcd = new cLcdTa7601 (rotate, info, mode);
   if (!lcd->initialise())
     return 0;
 
-  //{{{  font test
-  int height = 8;
-  while (height < 100) {
-    cPoint point;
-    lcd->clear (kMagenta);
-    for (char ch = 'A'; ch < 0x7f; ch++) {
-      point.x = lcd->text (kWhite, point, height, string(1,ch));
-      if (point.x > lcd->getWidth()) {
-        point.x = 0;
-        point.y += height;
-        if (point.y > lcd->getHeight())
-          break;
+  if (fontTest) {
+    //{{{  font test
+    int height = 8;
+    while (height < 100) {
+      cPoint point;
+      lcd->clear (kMagenta);
+      for (char ch = 'A'; ch < 0x7f; ch++) {
+        point.x = lcd->text (kWhite, point, height, string(1,ch));
+        if (point.x > lcd->getWidth()) {
+          point.x = 0;
+          point.y += height;
+          if (point.y > lcd->getHeight())
+            break;
+          }
         }
+
+      lcd->text (kYellow, cPoint(), 20, "Hello Colin");
+      lcd->present();
+      lcd->setBacklightOn();
+
+      lcd->delayUs (16000);
+      height += 4;
       }
-
-    lcd->text (kYellow, cPoint(), 20, "Hello Colin");
-    lcd->present();
-    lcd->setBacklightOn();
-
-    lcd->delayUs (16000);
-    height += 4;
     }
-  //}}}
+    //}}}
 
   // snapshot test
   while (true) {
