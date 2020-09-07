315f42a7ca6824d4a8434464ddc3554f81aae3b5
colinporth <colinporth@users.noreply.github.com>
2020-09-07 21:47:41 +0100

    Update cLcd.cpp

--- cLcd.cpp
+++ cLcd.cpp
@@ -4,6 +4,7 @@
 
 #include "cDrawAA.h"
 #include "cFrameDiff.h"
+#include "cSnapshot.h"
 
 #include <byteswap.h>
 
@@ -65,66 +66,6 @@ constexpr uint32_t k16WriteClrMask = k16WriteMask | k16DataMask;
 
 constexpr uint8_t kSpiBacklightGpio = 24;
 //}}}
-
-#include <bcm_host.h>
-
-static DISPMANX_DISPLAY_HANDLE_T mDisplay;
-static DISPMANX_MODEINFO_T mModeInfo;
-static DISPMANX_RESOURCE_HANDLE_T mSnapshot;
-static VC_RECT_T mVcRect;
-
-class cSnapshot {
-public:
-  //{{{
-  cSnapshot (const uint16_t width, const uint16_t height) : mWidth(width), mHeight(height) {
-
-    // dispmanx init
-    bcm_host_init();
-
-    mDisplay = vc_dispmanx_display_open (0);
-    if (!mDisplay) {
-      // error return
-      cLog::log (LOGERROR, "vc_dispmanx_display_open failed");
-      return;
-      }
-
-    if (vc_dispmanx_display_get_info (mDisplay, &mModeInfo)) {
-      // error return
-      cLog::log (LOGERROR, "vc_dispmanx_display_get_info failed");
-      return;
-      }
-
-    uint32_t imageHandle;
-    mSnapshot = vc_dispmanx_resource_create (VC_IMAGE_RGB565, width, height, &imageHandle);
-    if (!mSnapshot) {
-      // error return
-      cLog::log (LOGERROR, "vc_dispmanx_resource_create failed");
-      return;
-      }
-
-    vc_dispmanx_rect_set (&mVcRect, 0, 0, width, height);
-
-    cLog::log (LOGINFO, "display %dx%d", mModeInfo.width, mModeInfo.height);
-    }
-  //}}}
-  //{{{
-  ~cSnapshot() {
-    vc_dispmanx_resource_delete (mSnapshot);
-    vc_dispmanx_display_close (mDisplay);
-    }
-  //}}}
-
-  //{{{
-  void snap (uint16_t* frameBuf) {
-    vc_dispmanx_snapshot (mDisplay, mSnapshot, DISPMANX_TRANSFORM_T(0));
-    vc_dispmanx_resource_read_data (mSnapshot, &mVcRect, frameBuf, mWidth * 2);
-    }
-  //}}}
-
-private:
-  const uint16_t mWidth;
-  const uint16_t mHeight;
-  };
 
 // cLcd public
 //{{{
